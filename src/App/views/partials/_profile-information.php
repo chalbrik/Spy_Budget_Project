13002c17cebf51e03e0a2710d7f41e2da4e266d1
chalbrik <64196949+chalbrik@users.noreply.github.com>
2024-06-17 19:18:41 +0200

    Settigns page - basic elements added

---
+++ src/App/views/partials/_profile-information.php
@@ -0,0 +1,1 @@
+<div>Hello profile information</div>
--- src/App/views/settings.php
+++ src/App/views/settings.php
@@ -1,5 +1,13 @@
 <?php include $this->resolve("partials/_header.php") ?>
 
-<main>Settings - not avaible yet</main>
+<main>
+    <div class="settings-main-frame">
+        <div class="settings-fields">
+            <a class="settings-fields-name" href="/settings">Profile information</a>
+            <a class="settings-fields-name" href="">Transaction Categories</a>
+        </div>
+        <div class="settings-options"></div>
+    </div>
+</main>
 
 <?php include $this->resolve("partials/_footer.php") ?>
